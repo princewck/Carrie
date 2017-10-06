f1c8a5928d144178b962c6212a2ff2f220cf9256
wangchengkai <me@chengkai.wang>
2017-10-06 09:18:04 +0800

    config

--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,7 @@ const mysqlConfig = require('./mysql');
 
 const MYSQL = mysqlConfig;
 try {
-  const mysqlConfigLocal = fs.readFileSync('../.config');
+  const mysqlConfigLocal = fs.readFileSync('../.db');
   Object.assign(MYSQL, JSON.parse(mysqlConfigLocal.toString()));
 } catch (e) {
   console.info('no .config.json'); // eslint-disable-line no-console
